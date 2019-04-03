d711a8d574d235f8fee8d4b29b4485d73608a0cb
Pav0l <pavol.karas@gmail.com>
2019-04-03 15:02:18 +0200

    Setup Navbar, Login, Signup routes. Create Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route } from 'react-router-dom';
 
+// Components
+import Register from './components/Register';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        Hi :)
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Navbar />
+
+      <h3>Welcome to the home page</h3>
+
+      <Route path='/signup' component={Register} />
+
+      <Route path='/login' component={Login} />
+    </div>
+  );
 }
 
 export default App;
